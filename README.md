4c6b0de8cb9de6f760489f5b00a75be6d1cae22c
Sangjune Lee <aeeou154@@naver.com>
2023-01-29 17:41:40 +0900

    Props

--- README.md
+++ README.md
@@ -4,7 +4,10 @@
 
 ## 컴포넌트 만들기
 * Header, Nav, Article
-
+![image](https://user-images.githubusercontent.com/116176170/215314584-bfddcdad-ed37-4523-85c5-695ac8fc7cd8.png)
 ## netlify에 배포 하기
 * npx netlify-cli deploy -d build 
     * build 폴더에 있는 빌드된 파일을 배포
+
+## Props
+* 
--- src/App.js
+++ src/App.js
@@ -1,28 +1,38 @@
 import './App.css';
 
 function App() {
-  function Header() {
-    return <h1>React basic</h1>
+  function Header({title}) {
+    return <h1>title</h1>
   }
 
-  function Nav() {
+  function Nav({data}) {
+    const topicList = [];
+    // 배열을 받아서 반복문으로 풀고 
+    // 각각 li 태그로 감싸줌
+    // 이때, 리액트는 반복문을 사용하게되면 가상돔이기에 인식할 key가 필요 
+    for(let i=0; i<data.length; i++){
+      topicList.push(<li><a href={"/read/"+data[i].id}>{data[i].title}</a></li>)
+    }
     return <ul>
-      <li><a href='/read/1'>컴포넌트</a></li>
-      <li><a href='/read/2'>상태관리</a></li>
+      {topicList}
     </ul>
   }
-  function Article() {
+  function Article({title, body}) {
     return <>
-      <h2>Welcome</h2>
-      Hello, React
+      <h2>{title}</h2>
+      {body}
     </>
   }
+  const topics = [
+    {id:1, title:'컴포넌트', body:'컴포넌트는...'},
+    {id:2, title:'상태관리', body:'상태관리는...'},
+  ]
 
   return (
     <div className="App">
-      <Header></Header>
-      <Nav></Nav>
-      <Article></Article>
+      <Header title="React basic"></Header>
+      <Nav data={topics}></Nav>
+      <Article title="Welcome" body="Hello, React"></Article>
     </div>
   );
 }
