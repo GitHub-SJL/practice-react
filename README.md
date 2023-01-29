34d03b24d2b33168cf4faed50e71fcc6e284df81
Sangjune Lee <aeeou154@@naver.com>
2023-01-29 17:58:23 +0900

    state

--- README.md
+++ README.md
@@ -3,6 +3,8 @@
 ## HTML 코딩
 
 ## 컴포넌트 만들기
+* 컴포넌트는 독립적 단위의 모듈(높은 재사용성)
+* 페이지를 구성하는 최소단위
 * Header, Nav, Article
 ![image](https://user-images.githubusercontent.com/116176170/215314584-bfddcdad-ed37-4523-85c5-695ac8fc7cd8.png)
 ## netlify에 배포 하기
@@ -10,4 +12,11 @@
     * build 폴더에 있는 빌드된 파일을 배포
 
 ## Props
-* 
+* 컴포넌트에 원하는 값을 넘겨줄때 사용
+    * 변수,함수,객체, 배열 등
+    * 컴포넌트의 재사용을 위해 사용
+![image](https://user-images.githubusercontent.com/116176170/215315879-eab47c35-cd3c-49b5-a2f1-46d1079aec7a.png)
+
+## State
+* 컴포넌트 내에서 유동적으로 변할 수 있는 값을 저장
+* 자바스크립트에서 처럼 let으로 값을 바꾸면 적용 안됨
--- src/App.js
+++ src/App.js
@@ -1,31 +1,50 @@
+
+import { useState } from 'react';
 import './App.css';
 
 function App() {
-  function Header({title}) {
-    return <h1>title</h1>
+  function Header({ title }) {
+    return <h1>{title}</h1>
   }
 
-  function Nav({data}) {
+  function Link() {
+    let countState = useState(1000);
+    let count = countState[0];
+    let setCount = countState[1];
+
+    const clickHandler = () => {
+      return setCount((count) => count + 1)
+    }
+
+
+    return <div>
+      <button onClick={clickHandler}>{count}</button>
+    </div>
+  }
+
+
+
+  function Nav({ data }) {
     const topicList = [];
     // 배열을 받아서 반복문으로 풀고 
     // 각각 li 태그로 감싸줌
     // 이때, 리액트는 반복문을 사용하게되면 가상돔이기에 인식할 key가 필요 
-    for(let i=0; i<data.length; i++){
-      topicList.push(<li><a href={"/read/"+data[i].id}>{data[i].title}</a></li>)
+    for (let i = 0; i < data.length; i++) {
+      topicList.push(<li><a href={"/read/" + data[i].id}>{data[i].title}</a></li>)
     }
     return <ul>
       {topicList}
     </ul>
   }
-  function Article({title, body}) {
+  function Article({ title, body }) {
     return <>
       <h2>{title}</h2>
       {body}
     </>
   }
   const topics = [
-    {id:1, title:'컴포넌트', body:'컴포넌트는...'},
-    {id:2, title:'상태관리', body:'상태관리는...'},
+    { id: 1, title: '컴포넌트', body: '컴포넌트는...' },
+    { id: 2, title: '상태관리', body: '상태관리는...' },
   ]
 
   return (
@@ -33,6 +52,7 @@ function App() {
       <Header title="React basic"></Header>
       <Nav data={topics}></Nav>
       <Article title="Welcome" body="Hello, React"></Article>
+      <Link></Link>
     </div>
   );
 }
