2f04b72271167b9d3cee620786d83d3fc9ddbeef
Sangjune Lee <aeeou154@@naver.com>
2023-01-29 17:12:15 +0900

    컴포넌트 만들기

--- README.md
+++ README.md
@@ -1,3 +1,6 @@
 # 리액트 쌩 기초
 
 ## HTML 코딩
+
+## 컴포넌트 만들기
+* Header, Nav, Article
--- src/App.js
+++ src/App.js
@@ -1,15 +1,28 @@
 import './App.css';
 
 function App() {
-  return (
-    <div className="App">
-      <h1>React basic</h1>
-      <ul>
-        <li><a href='/read/1'>컴포넌트</a></li>
-        <li><a href='/read/2'>상태관리</a></li>
-      </ul>
+  function Header() {
+    return <h1>React basic</h1>
+  }
+
+  function Nav() {
+    return <ul>
+      <li><a href='/read/1'>컴포넌트</a></li>
+      <li><a href='/read/2'>상태관리</a></li>
+    </ul>
+  }
+  function Article() {
+    return <>
       <h2>Welcome</h2>
       Hello, React
+    </>
+  }
+
+  return (
+    <div className="App">
+      <Header></Header>
+      <Nav></Nav>
+      <Article></Article>
     </div>
   );
 }
