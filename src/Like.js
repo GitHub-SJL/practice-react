a26df05222bd0e76ae0b8a76bc2c4eb06d0841a0
Sangjune Lee <aeeou154@@naver.com>
2023-01-29 18:49:02 +0900

    state함수 여러번 사용 시 주의할점

--- src/Like.js
+++ src/Like.js
@@ -2,11 +2,26 @@ import { useState } from 'react';
 
 function Link() {
     const [count, setCount] = useState(0);
+    // 리액트에서 이벤트의 값은 반드시 함수이다.
 
     const clickHandler = () => {
-      return setCount((count) => count + 1)
-    }
+    // 클릭했을때 +3이 되려고 할때 아래처럼 작성하면 안된다.
+    // 다음 상태의 +1이아닌 서로 현재 상태의 +1을 하려고 동작하게된다. 
+    //   setCount(count + 1)
+    //   setCount(count + 1)
+    //   setCount(count + 1)
+    setCount((oldState)=>{
+        return oldState + 1;
+    })
+    setCount((oldState)=>{
+        return oldState + 1;
+    })
+    setCount((oldState)=>{
+        return oldState + 1;
+    })
 
+    }
+ 
 
     return <div>
       <button onClick={clickHandler}>{count}</button>
